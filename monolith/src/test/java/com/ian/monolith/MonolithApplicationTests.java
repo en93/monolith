994dd5434d1b3943223c6e23f9c33850ecef8617
Ian <ianbabington93@gmail.com>
2022-04-05 20:44:19 +1200

    Initial commit, from spring-boot

---
+++ monolith/src/test/java/com/ian/monolith/MonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
