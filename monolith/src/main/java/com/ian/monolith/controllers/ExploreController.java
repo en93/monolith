324e5584fe7e900a5ef82d4cbf87e67a732b8496
Ian <ianbabington93@gmail.com>
2022-04-06 23:15:16 +1200

    Forms and all

--- monolith/src/main/java/com/ian/monolith/controllers/ExploreController.java
+++ monolith/src/main/java/com/ian/monolith/controllers/ExploreController.java
@@ -1,27 +1,38 @@
 package com.ian.monolith.controllers;
 
 import com.ian.monolith.CityRepository;
+import com.ian.monolith.form.CitySelection;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(value={"/", "explore"})
+@RequestMapping("/")
 public class ExploreController {
 
     @GetMapping
     public String explore(Model model){
-        model.addAttribute("name", "Ian");
+//        model.addAttribute("name", "Ian");
         model.addAttribute("cities", CityRepository.getAllCities());
+        model.addAttribute("cityselection", new CitySelection());
         return "explore";
     }
 
-    @PutMapping
-    public String exploreCity(Model model){
-        model.addAttribute("name", "NOt Ian");
+    @PostMapping
+    public String redirectToExploreCity(@ModelAttribute CitySelection citySelection){
+        return "redirect:/" + citySelection.getCity();
+    }
+
+    @GetMapping("/{city}")
+    public String exploreCity(Model model, @PathVariable("city")  String city){
+        //How to handle this situation better?
+        CitySelection selection = new CitySelection();
+        selection.setCity(city);
+
+//        model.addAttribute("name", "Not Ian");
+        model.addAttribute("user_city", city);
         model.addAttribute("cities", CityRepository.getAllCities());
+        model.addAttribute("cityselection", selection);
         return "explore";
     }
 }
---
+++ monolith/src/main/java/com/ian/monolith/form/CitySelection.java
@@ -0,0 +1,14 @@
+package com.ian.monolith.form;
+
+public class CitySelection {
+
+    public String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
